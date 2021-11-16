0fe8adba7804c2a167f60d8b95c523e5b608f955
coronadito <alejandra.coronadojl@gmail.com>
2021-11-15 20:50:10 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,70 @@
+from flask import Flask, request
+import json
+from twilio.rest import Client
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+ 
+#Creamos objeto Flask
+app = Flask(__name__)
+ 
+#Cargamos la informacion de nuestro archivo config.json
+f = open("config.json", "r")
+env = json.loads(f.read())
+ 
+#Creamos nuestro primer servicio web
+@app.route('/test', methods=['GET'])
+def test():
+    return "hello world"
+
+@app.route('/send_sms', methods=['POST'])
+def send_sms():
+    try:
+        #variables confi
+        account_sid = env['TWILIO ACCOUNT SID']
+        auth_token = env['TWILIO AUTH TOKEN']
+        origen = env['TWILIOPHONE NUMBER']
+        client = Client(account_sid, auth_token)
+        data = request.json
+        contenido = data["contenido"]
+        destino = data["destino"]
+       #envia el mensaje
+        message = client.messages.create(
+                            body=contenido,
+                            from_=origen,
+                            to='+57' + destino
+                        )
+        print(message)
+        return "send success"
+    except Exception as e:
+        print(e)
+        return "error"
+
+
+@app.route('/send_email', methods=['POST'])
+def send_email():
+    data = request.json
+    contenido = data["contenido"]
+    destino = data["destino"]
+    asunto = data["asunto"]
+    print(contenido, destino, asunto)
+    message = Mail(
+    from_email= env['SENGRID EMAIL FROM'],
+    to_emails= destino,
+    subject= asunto,
+    html_content= contenido)
+    try:
+        sg = SendGridAPIClient(env['SENGRID API KEY'])
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+        return "send success"
+    except Exception as e:
+        print(e)
+        return "error"
+
+
+ 
+#Ejecutamos el servidor
+if __name__ == '__main__':
+    app.run()
